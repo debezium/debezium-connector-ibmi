7c487932c3aa72fde645319c69980fc01a1b110d
Aziza Kalmasova <aziza.kalmasova@glowbyteconsulting.com>
2024-09-17 09:04:55 +0200

    DBZ-8300 Test with pub400

--- debezium-connector-ibmi/src/test/java/io/debezium/connector/db2as400/As400ConnectorIT.java
+++ debezium-connector-ibmi/src/test/java/io/debezium/connector/db2as400/As400ConnectorIT.java
@@ -9,14 +9,18 @@ import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.debezium.connector.db2as400.util.TestHelper;
 import io.debezium.embedded.AbstractConnectorTest;
+import io.debezium.jdbc.JdbcConnection;
 import io.debezium.util.Testing;
 
 public class As400ConnectorIT extends AbstractConnectorTest {
+    private static final Logger log = LoggerFactory.getLogger(As400ConnectorIT.class);
 
-    private static final String TABLE = "TESTT";
+    private static final String TABLE = "TEST_TABLE";
 
     @Before
     public void before() throws SQLException {
@@ -29,6 +33,7 @@ public class As400ConnectorIT extends AbstractConnectorTest {
     @Test
     public void shouldSnapshotAndStream() throws Exception {
         Testing.Print.enable();
+        // Testing.Debug.enable();
         final var config = TestHelper.defaultConfig(TABLE);
 
         start(As400RpcConnector.class, config);
@@ -37,11 +42,21 @@ public class As400ConnectorIT extends AbstractConnectorTest {
         // Wait for snapshot completion
         var records = consumeRecordsByTopic(1);
 
-        TestHelper.testConnection().execute(
-                "INSERT INTO " + TABLE + " VALUES (2, 'second')",
+        records.print();
+
+        // JdbcConnection conn = TestHelper.testConnection().setAutoCommit(true);
+        // conn.execute("INSERT INTO " + TABLE + " VALUES (2, 'second')",
+        // "INSERT INTO " + TABLE + " VALUES (3, 'third')");
+
+        JdbcConnection conn = TestHelper.testConnection().setAutoCommit(false);
+        conn.executeWithoutCommitting("INSERT INTO " + TABLE + " VALUES (2, 'second')",
                 "INSERT INTO " + TABLE + " VALUES (3, 'third')");
+        conn.rollback();
+        // conn.commit();
 
         records = consumeRecordsByTopic(2);
+
+        records.print();
 
         assertNoRecordsToConsume();
         stopConnector();
