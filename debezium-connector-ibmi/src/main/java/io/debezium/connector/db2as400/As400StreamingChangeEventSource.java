ff2379c8e4a19cc484705e0def0eb4c5876fe2d1
Jiri Pechanec <jpechane@redhat.com>
2024-10-08 06:06:42 +0200

    DBZ-8300 Fix formatting

--- debezium-connector-ibmi/src/main/java/io/debezium/connector/db2as400/As400StreamingChangeEventSource.java
+++ debezium-connector-ibmi/src/main/java/io/debezium/connector/db2as400/As400StreamingChangeEventSource.java
@@ -8,7 +8,13 @@ package io.debezium.connector.db2as400;
 import java.io.IOException;
 import java.sql.SQLNonTransientConnectionException;
 import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
